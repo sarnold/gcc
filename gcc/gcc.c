057c9a2cc71affd7873487551b0799dd89f19645
Khem Raj <raj.khem@gmail.com>
2013-03-29 09:22:00 +0400

    cpp: honor sysroot.
    Currently, if the gcc toolchain is relocated and installed from sstate, then you try and compile
    preprocessed source (.i or .ii files), the compiler will try and access the builtin sysroot location
    rather than the --sysroot option specified on the commandline. If access to that directory is
    permission denied (unreadable), gcc will error.
    
    This happens when ccache is in use due to the fact it uses preprocessed source files.
    
    The fix below adds %I to the cpp-output spec macro so the default substitutions for -iprefix,
    -isystem, -isysroot happen and the correct sysroot is used.
    
    [YOCTO #2074]
    
    RP 2012/04/13
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    
    Upstream-Status: Pending

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -1329,7 +1329,7 @@ static const struct compiler default_compilers[] =
 					   %W{o*:--output-pch=%*}}%V}}}}}}}", 0, 0, 0},
   {".i", "@cpp-output", 0, 0, 0},
   {"@cpp-output",
-   "%{!M:%{!MM:%{!E:cc1 -fpreprocessed %i %(cc1_options) %{!fsyntax-only:%(invoke_as)}}}}", 0, 0, 0},
+   "%{!M:%{!MM:%{!E:cc1 -fpreprocessed %i %I %(cc1_options) %{!fsyntax-only:%(invoke_as)}}}}", 0, 0, 0},
   {".s", "@assembler", 0, 0, 0},
   {"@assembler",
    "%{!M:%{!MM:%{!E:%{!S:as %(asm_debug) %(asm_options) %i %A }}}}", 0, 0, 0},
