8b18e268d2a5f2117a6af5bd67c8504380386d5a
Khem Raj <raj.khem@gmail.com>
2015-04-28 23:18:39 -0700

    aarch64: Add support for musl ldso
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- gcc/config/aarch64/aarch64-linux.h
+++ gcc/config/aarch64/aarch64-linux.h
@@ -23,6 +23,8 @@
 
 #define GLIBC_DYNAMIC_LINKER  SYSTEMLIBS_DIR "ld-linux-aarch64%{mbig-endian:_be}%{mabi=ilp32:_ilp32}.so.1"
 
+#define MUSL_DYNAMIC_LINKER  SYSTEMLIBS_DIR "ld-musl-aarch64.so.1"
+
 #undef  ASAN_CC1_SPEC
 #define ASAN_CC1_SPEC "%{%:sanitize(address):-funwind-tables}"
 
